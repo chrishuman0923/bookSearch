706c5b957db5f627ad5005a2039990df3de8d2e1
chuman <42072991+chrishuman0923@users.noreply.github.com>
2019-01-24 22:42:39 -0500

    Working

--- server/models/book.js
+++ server/models/book.js
@@ -5,8 +5,9 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now },
+  description: String,
+  image: { type: String, required: true },
+  link: { type: String, required: true },
 });
 
 const Book = mongoose.model('Book', bookSchema);
