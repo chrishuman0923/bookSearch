ef2ed4a805cc5a08c3ecbfac7ff1d0978fcaee44
chuman <42072991+chrishuman0923@users.noreply.github.com>
2019-01-25 00:04:46 -0500

    Add path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
@@ -11,7 +12,7 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 }
 
 // Add routes, both API and view
