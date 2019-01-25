dea8edaf951790e5ebc8da57e8cbbcb7851fe41c
chuman <42072991+chrishuman0923@users.noreply.github.com>
2019-01-25 10:02:43 -0500

    Move backend into server directory

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 }
 
 // Add routes, both API and view
